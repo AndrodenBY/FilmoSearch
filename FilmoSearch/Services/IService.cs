134c6044971b77a07b2d8286fd165fcb3c566a8e
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2025-04-12 01:58:21 +0300

    refactor(api): follow up commit to bettering naming conventions

--- FilmoSearch/Services/IService.cs
+++ FilmoSearch/Services/IService.cs
@@ -1,6 +1,6 @@
 ﻿namespace FilmoSearch.Services
 {
-    public interface IFilmoSearchService<T> where T : class
+    public interface IService<T> where T : class
     {
         Task<IEnumerable<T>?> GetAllAsync();
         Task<T?> GetByIdAsync(Guid id);
